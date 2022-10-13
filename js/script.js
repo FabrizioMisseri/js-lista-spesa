32c92944f7572f69a60d5edfa3d6c2750c0bba53
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-13 13:28:03 +0200

    drug list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Consegna:
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+const list = [
+                "Cannabis indica 25g",
+                "Cannabis sativa 2g",
+                "Cannabis ruderalis 3g",
+                "Critical Kush 6g",
+                "Cream Caramel 1g",
+                "Black Domina 10g"
+            ];
+
+const drugs = document.querySelector("ul");
+
+let i = 0; 
+while (i < list.length) {
+    let drug = (list[i]);
+    drugs.innerHTML += ` <li>${drug}</li> `;
+    i++
+}
+
+
+
 // Consigli:
 // - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
